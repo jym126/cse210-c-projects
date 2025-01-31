b07ae6e49a2331ccc87b7360be41870bd1d0f553
jym126 <jym126@hotmail.com>
2025-01-31 21:00:14 +0100

    ordering classes

---
+++ week04/OnlineOrdering/Order.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+
+public class Order
+{
+    private List<Product> _products;
+    private Customer _customer;
+
+    public Order(Customer customer)
+    {
+        _customer = customer;
+        _products = new List<Product>();
+    }
+
+    public void AddProduct(Product product)
+    {
+        _products.Add(product);
+    }
+
+    public decimal GetTotalPrice()
+    {
+        decimal totalCost = 0;
+        foreach (Product product in _products)
+        {
+            totalCost += product.GetTotalCost();
+        }
+
+        decimal shippingCost = _customer.IsInUSA() ? 5m : 35m;
+        return totalCost + shippingCost;
+    }
+
+    public string GetPackingLabel()
+    {
+        string label = "Packing Label:\n";
+        foreach (Product product in _products)
+        {
+            label += $"{product.GetName()} (ID: {product.GetProductId()})\n";
+        }
+        return label;
+    }
+
+    public string GetShippingLabel()
+    {
+        string label = "Shipping Label:\n";
+        label += $"{_customer.GetName()}\n";
+        label += _customer.GetAddress().GetFullAddress();
+        return label;
+    }
+}
