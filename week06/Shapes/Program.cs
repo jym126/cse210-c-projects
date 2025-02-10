db870f8f19442af4c1df443384c775de47909353
jym126 <jym126@hotmail.com>
2025-02-10 19:04:31 +0100

    shapes

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -1,9 +1,85 @@
 using System;
+using System.Collections.Generic;
+
+abstract class Shape
+{
+    public string Color { get; set; }
+
+    public Shape(string color)
+    {
+        Color = color;
+    }
+
+    // Abstract method to be implemented in derived classes
+    public abstract double GetArea();
+
+    public void DisplayInfo()
+    {
+        Console.WriteLine($"Color: {Color}, Area: {GetArea():F2}");
+    }
+}
+
+class Square : Shape
+{
+    public double Side { get; set; }
+
+    public Square(string color, double side) : base(color)
+    {
+        Side = side;
+    }
+
+    public override double GetArea()
+    {
+        return Side * Side;
+    }
+}
+
+class Rectangle : Shape
+{
+    public double Width { get; set; }
+    public double Height { get; set; }
+
+    public Rectangle(string color, double width, double height) : base(color)
+    {
+        Width = width;
+        Height = height;
+    }
+
+    public override double GetArea()
+    {
+        return Width * Height;
+    }
+}
+
+class Circle : Shape
+{
+    public double Radius { get; set; }
+
+    public Circle(string color, double radius) : base(color)
+    {
+        Radius = radius;
+    }
+
+    public override double GetArea()
+    {
+        return Math.PI * Radius * Radius;
+    }
+}
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        List<Shape> shapes = new List<Shape>
+        {
+            new Square("Red", 4),
+            new Rectangle("Blue", 5, 3),
+            new Circle("Green", 2.5)
+        };
+
+        foreach (var shape in shapes)
+        {
+            shape.DisplayInfo();
+        }
     }
 }
